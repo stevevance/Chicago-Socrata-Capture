9a33dd546627d738845da84dbceaae91fa240b70
Steven Vance <steve@stevevance.net>
2014-05-20 01:26:09 -0500

    a good place to commit

--- view.php
+++ view.php
@@ -3,12 +3,20 @@ require_once("psql.php");
 require_once("class.windy.php");
 
 showForm();
-
-
-//storeView("ydr8-5enu","building permits");
-//checkViewExistence("ydr8-5enu");
+confirmTableDelete();
 
 // ydr8-5enu is building permits
+// 22u3-xenr is building violations
+
+/* 
+APPLICATION VARIABLES
+*/
+$maxrows = 1000;
+
+/*
+DON'T EDIT BELOW THIS LINE 
+========
+*/
 
 function showForm() {
 	$f = "<h1>Chicago Socrata Capture</h1><h2>Copy \"views\" from the City of Chicago data portal to PostgreSQL</h2>";
@@ -31,6 +39,7 @@ IF NOT EXISTS info (
 	id SERIAL,
 	view TEXT,
 	description TEXT,
+	name TEXT,
 	PRIMARY KEY (id),
 	UNIQUE(view)
 )";
@@ -40,47 +49,60 @@ IF NOT EXISTS info (
 }
 
 function checkViewExistence($view) {
-	$chicago = new windy('city');
+	$chicago = new windy('city','json','object',null,false); // the final param is true/false for debugging
 	
 	$check = $chicago->getViewsByID($view);
+	$tableName = createTableName($view);
 	
 	echo "<pre>";
 	//var_dump($check);
 	echo "</pre>";
 	
 	if(is_numeric($check->createdAt)) {
-		storeView($view, $check->description);
+		storeView($view, $check, $tableName);
 		
 		//displayColumns($check->columns); // comment this line if you don't want to display the columns info
-		createTable($chicago, $check, $view);
+		createTable($chicago, $check, $view, $tableName);
 	} else {
-		message("View ".$view." doesn't seem to exist",false);
+		message("View ".$view." doesn't seem to exist in the data portal",false);
 	}
 }
 
-function createTable($viewObject, $result, $view) {
+function createTableName($view) {
+	$tableName = "view_".str_replace("-", "_", $view);
+	return $tableName;
+}
+
+function createTable($viewObject, $result, $view, $tableName) {
 	global $plink;
 	
-	$tableName = "view_".str_replace("-", "_", $view);
 	$columns = array();
 	
 	$columnsArray = $result->columns;
+	$addOid = 0;
 	foreach($columnsArray as $column) {
-		//if($column->fieldName != "id") {
+		if($column->fieldName != "id") {
 			$columns[] = strtolower($column->fieldName)." ".strtoupper(choosePsqlColumnType($column->dataTypeName));
-		//}
+			$addOid++;
+		} else {
+			$columns[] = "id SERIAL";
+			$columns[] = "PRIMARY KEY (id)";
+			$addOid = -999; // if the id field exists, use that instead of making an oid field
+		}
+	}
+	if($addOid > 0) {
+		$columns[] = "oid SERIAL";
+		$columns[] = "PRIMARY KEY (oid)";
 	}
 	$columnsPsql = implode(", ",$columns);
 
 	$psql = "CREATE TABLE
 IF NOT EXISTS $tableName (
-	oid SERIAL,
 	insert_time TIMESTAMP,
-	$columnsPsql, 
-	PRIMARY KEY (oid)
+	$columnsPsql
 )";
 
-	//echo $psql;
+	echo $psql;
 	
 	$result = pg_prepare($plink, "", $psql);
 	$result = pg_execute($plink, "",array()) or die(message("Couldn't create the table for view: $view"));
@@ -89,7 +111,7 @@ IF NOT EXISTS $tableName (
 }
 
 function copyViewIntoTable($view, $viewObject, $tableName, $columnsArray) {
-	global $plink;
+	global $plink, $maxrows;
 	
 	$rows = $viewObject->getRows($view,true,1000,null,null,false,null,null,null);
 	
@@ -111,7 +133,7 @@ function copyViewIntoTable($view, $viewObject, $tableName, $columnsArray) {
 	foreach($rows->data as $row) {
 		$psql = "INSERT INTO $tableName (insert_time, $columnsPsql) VALUES (";
 		$rowData = array();
-		$i=8; // this is a workaround; for some reason the first 8 rows of Building Violations (view 22u3-xenr) are gobbledy-gook not defined/seen when displaying columns; same with FOIA for Mayor's Office People with Disabilities (view fazv-a8mb)
+		$i=8; // this is a workaround; for some reason the first 8 rows of Building Violations (view 22u3-xenr) are gobbledy-gook which are not defined/seen when displaying columns; same with FOIA for Mayor's Office People with Disabilities (view fazv-a8mb)
 		foreach($columns as $column) {
 			if(empty($row[$i])) {
 				//$row[$i] = "null";
@@ -128,11 +150,33 @@ function copyViewIntoTable($view, $viewObject, $tableName, $columnsArray) {
 	
 	$result = pg_query($plink, $queries);
 	if (!$result) {
-		message("Couldn't copy the rows from the view to table: $tableName");
+		message("Couldn't copy the rows from the view to table: $tableName. ".deleteTableLink($view));
+		confirmTableDelete($view);
 		exit;
 	} else {
 		message("Copied rows from the view to table: $tableName",false);
 	}
+}
+
+function confirmTableDelete($view = null) {
+	global $plink;
+	if(isset($_GET['delete'])) {
+		if(!empty($_GET['delete'])) {
+			$view = $_GET['delete'];
+		}
+		$result = pg_query($plink, "DROP TABLE ".createTableName($view));
+		if($result) {
+			message("Table: ".createTableName($view)." deleted",false);
+		}
+		$result = pg_query($plink, "DELETE FROM info WHERE view = '$view'");
+		if($result) {
+			message("View: ".createTableName($view)." removed from info table",false);
+		}
+	}
+}
+
+function deleteTableLink($view = null) {
+	echo  "<a href='?delete=$view'>Delete table: ".createTableName($view)."?</a>";
 }
 
 function displayColumns($columnsArray) {
@@ -149,18 +193,19 @@ function displayColumns($columnsArray) {
 	echo "</pre>";
 }
 
-function storeView($view, $description) {
+function storeView($view, $result, $tableName) {
 	global $plink;
 	
 	$view = pg_escape_string($view);
-	$description = pg_escape_string($description);
+	$description = pg_escape_string($result->description);
+	$name = pg_escape_string($result->name);
 	
-	$psql = "INSERT INTO info (view, description) VALUES ('{$view}','{$description}')";
+	$psql = "INSERT INTO info (view, description, name) VALUES ('{$view}','{$description}','{$name}')";
 	
 	$result = pg_prepare($plink, "", $psql);
-	$result = pg_execute($plink, "",array()) or die(message("Couldn't save the view ID. Have you already saved this view ID?"));
+	$result = pg_execute($plink, "",array()) or die(message("Couldn't save the view ID. Have you already saved this view ID? ".deleteTableLink($view)));
 	
-	message("View added to our info table",false);
+	message("View: $view added to our info table",false);
 }
 
 function message($msg, $error = true) {
